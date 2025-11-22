70e7f579d8bf5719814ae4fd42f8f8c1bd58c6b1
UDDHAB CHARAN DAS <142538347+uddhabdas@users.noreply.github.com>
2025-11-22 21:06:48 +0530

    Update socket.ts

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,11 +1,7 @@
-"use client";
-
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  process.env.NEXT_PUBLIC_SOCKET_URL ||
-  "https://eduhive-server.onrender.com";
-
-export const socket = io(SOCKET_URL, {
+export const socket = io(process.env.NEXT_PUBLIC_API_URL!, {
   transports: ["websocket"],
+  reconnection: true,
+  reconnectionAttempts: 10,
 });
